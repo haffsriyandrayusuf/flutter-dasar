eb5a6b613d4c1f12457e17c611b07e0c30708e40
Haffsriyandra Yusuf <haffsriyandrayusuf@protonmail.com>
2022-03-15 17:28:44 +0700

    flutter_13_text_field_widget : control

--- flutter_13_text_field_widget/lib/main.dart
+++ flutter_13_text_field_widget/lib/main.dart
@@ -4,9 +4,16 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController myController = TextEditingController();
+  String hasilInput = 'HASIL INPUT';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,74 +22,96 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Latihan Text Field'),
         ),
-        body: const Center(
+        body: Center(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: TextField(
-              // ! FEATURES TEXTFIELD
-              // autocorrect: true,
-              // autofocus: true,
-              // enableSuggestions: true,
-              // enableInteractiveSelection: true,
-              // enabled: false,
-              // obscureText: true,
-              // obscuringCharacter: '#',
-              // keyboardType: TextInputType.text,
-              // readOnly: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  // ! FEATURES TEXTFIELD
+                  // autocorrect: true,
+                  // autofocus: true,
+                  // enableSuggestions: true,
+                  // enableInteractiveSelection: true,
+                  // enabled: false,
+                  // obscureText: true,
+                  // obscuringCharacter: '#',
+                  // keyboardType: TextInputType.text,
+                  // readOnly: true,
 
-              // ! DECORATION TEXTFIELD
-              textAlign: TextAlign.start,
-              textAlignVertical: TextAlignVertical.top,
-              textCapitalization: TextCapitalization.characters,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 24,
-              ),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.amber,
-                icon: Icon(
-                  Icons.people,
-                  size: 35,
-                  color: Colors.red,
+                  // ! DECORATION TEXTFIELD
+                  // textAlign: TextAlign.start,
+                  // textAlignVertical: TextAlignVertical.top,
+                  // textCapitalization: TextCapitalization.characters,
+                  // style: TextStyle(
+                  //   color: Colors.red,
+                  //   fontSize: 24,
+                  // ),
+                  // decoration: InputDecoration(
+                  //   filled: true,
+                  //   fillColor: Colors.amber,
+                  //   icon: Icon(
+                  //     Icons.people,
+                  //     size: 35,
+                  //     color: Colors.red,
+                  //   ),
+                  //   border: OutlineInputBorder(),
+                  //   focusedBorder: OutlineInputBorder(
+                  //     borderSide: BorderSide(
+                  //       color: Colors.red,
+                  //     ),
+                  //   ),
+                  //   enabledBorder: OutlineInputBorder(
+                  //     borderSide: BorderSide(
+                  //       color: Colors.blue,
+                  //     ),
+                  //   ),
+                  //   hintText: 'Input Your Name',
+                  //   hintStyle: TextStyle(
+                  //     color: Colors.red,
+                  //     fontSize: 24,
+                  //   ),
+                  //   labelText: 'Full Name',
+                  //   labelStyle: TextStyle(
+                  //     color: Colors.red,
+                  //     fontSize: 24,
+                  //   ),
+                  //   prefixText: 'Name : ',
+                  //   prefixIcon: Icon(
+                  //     Icons.person_add,
+                  //     size: 35,
+                  //   ),
+                  //   prefixStyle: TextStyle(
+                  //     color: Colors.red,
+                  //     fontSize: 24,
+                  //   ),
+                  //   prefixIconColor: Colors.black,
+                  //   suffixIcon: IconButton(
+                  //     icon: Icon(Icons.remove_red_eye),
+                  //     onPressed: null,
+                  //   ),
+                  //   suffixIconColor: Colors.red,
+                  // ),
+
+                  // ! CONTROLTEXTFIELD
+                  controller: myController,
+                  onChanged: (value) {
+                    print('ONCHANGE');
+                  },
+                  onSubmitted: (value) {
+                    print('EDIT SUCCESS');
+                    setState(() {
+                      hasilInput = value;
+                    });
+                  },
+                  // onEditingComplete: () {
+                  // print('EDIT SUCCESS');
+                  // print(myController.text);
+                  // },
                 ),
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.red,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                  ),
-                ),
-                hintText: 'Input Your Name',
-                hintStyle: TextStyle(
-                  color: Colors.red,
-                  fontSize: 24,
-                ),
-                labelText: 'Full Name',
-                labelStyle: TextStyle(
-                  color: Colors.red,
-                  fontSize: 24,
-                ),
-                prefixText: 'Name : ',
-                prefixIcon: Icon(
-                  Icons.person_add,
-                  size: 35,
-                ),
-                prefixStyle: TextStyle(
-                  color: Colors.red,
-                  fontSize: 24,
-                ),
-                prefixIconColor: Colors.black,
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.remove_red_eye),
-                  onPressed: null,
-                ),
-                suffixIconColor: Colors.red,
-              ),
+                Text(hasilInput),
+              ],
             ),
           ),
         ),
